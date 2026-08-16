19ad7b134a1346180ed803d24db81bd6334d3b1e
YALLICO OWNER <yallico2024@gmail.com>
2026-08-16 18:06:06 -0500

    Actualizar tienda.js

--- tienda.js
+++ tienda.js
@@ -3,12 +3,10 @@ function cargarTienda() {
     contenedor.innerHTML = '';
     PRODUCTOS_TIENDA.forEach(prod => {
         contenedor.innerHTML += `
-            <div class="product-card">
-                <img src="${prod.imagen}" alt="${prod.nombre}" loading="lazy">
+            <div class="product-card" onclick='abrirProducto(${JSON.stringify(prod)})'>
+                <img src="${prod.imagen}" alt="${prod.nombre}">
                 <h4>${prod.nombre}</h4>
-                <p>${prod.descripcion}</p>
-                <div class="precio">${prod.precio}</div>
-                <a href="https://wa.me/51927174369?text=${encodeURIComponent(prod.whatsapp)}" class="btn-comprar" target="_blank">Comprar</a>
+                <div class="precio">${prod.precio.split('|')[0]}</div>
             </div>
         `;
     });
