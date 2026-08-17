912ddfe2240e31380906495f3e3f94c2219f7ab7
YALLICO OWNER <yallico2024@gmail.com>
2026-08-16 19:42:26 -0500

    Crear monedas.js

---
+++ monedas.js
@@ -0,0 +1,10 @@
+const monedas = {
+  PEN: {symbol: 'S/', rate: 1},
+  USD: {symbol: '$', rate: 0.267},
+  CLP: {symbol: '$', rate: 250},
+  ARS: {symbol: '$', rate: 400},
+  MXN: {symbol: '$', rate: 5},
+  BOB: {symbol: '$', rate: 3},
+  UYU: {symbol: '$', rate: 10},
+  COP: {symbol: '$', rate: 1000}
+};
