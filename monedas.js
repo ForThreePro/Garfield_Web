04595971f45a1f602aa9b8155906102225781b84
YALLICO OWNER <yallico2024@gmail.com>
2026-08-16 19:50:59 -0500

    Actualizar monedas.js

--- monedas.js
+++ monedas.js
@@ -1,10 +1,10 @@
 const monedas = {
-  PEN: {symbol: 'S/', rate: 1},
-  USD: {symbol: '$', rate: 0.267},
-  CLP: {symbol: '$', rate: 250},
-  ARS: {symbol: '$', rate: 400},
-  MXN: {symbol: '$', rate: 5},
-  BOB: {symbol: '$', rate: 3},
-  UYU: {symbol: '$', rate: 10},
-  COP: {symbol: '$', rate: 1000}
+  PEN: {symbol: 'S/', rate: 1, name: 'Soles PE'},
+  USD: {symbol: '$', rate: 0.267, name: 'Dolares US'},
+  CLP: {symbol: '$', rate: 250, name: 'Pesos CL'},
+  ARS: {symbol: '$', rate: 400, name: 'Pesos AR'},
+  MXN: {symbol: '$', rate: 5, name: 'Pesos MX'},
+  BOB: {symbol: 'Bs', rate: 3, name: 'Bolivianos'},
+  UYU: {symbol: '$', rate: 10, name: 'Pesos UY'},
+  COP: {symbol: '$', rate: 1000, name: 'Pesos CO'}
 };
